ff5288c64f3a1d0f72c8aedccf8d224d29c0267b
mamazinarostov <rostovmamazina@yandex.ru>
2024-07-23 12:23:36 +0700

    Update main.py

--- main.py
+++ main.py
@@ -10,43 +10,46 @@ def webhook():
         # Логирование всех данных запроса
         app.logger.info("Request headers: %s", request.headers)
         app.logger.info("Request data: %s", request.data)
-        
-        # Обработка тестового запроса от Tilda
-        if request.form.get('test') == 'test':
-            app.logger.info("Received test request from Tilda")
-            return jsonify({'status': 'success', 'message': 'Test request received successfully'}), 200
-        
-        # Извлечение JSON данных
-        data = request.get_json()
-        if not data:
-            app.logger.error("No JSON data found in the request")
-            return jsonify({'status': 'error', 'message': 'Invalid JSON data'}), 400
-        
-        app.logger.info("Received JSON data: %s", data)
-        
-        # Проверка наличия ключа "Phone" в каждом элементе массива JSON
-        phone_numbers = [item.get('Phone') for item in data if 'Phone' in item]
-        
-        if not phone_numbers:
-            app.logger.error("Phone number not found in the JSON data")
-            return jsonify({'status': 'error', 'message': 'Phone number is required'}), 400
 
-        phone_number = phone_numbers[0]  # Берем первый номер телефона из списка
-        app.logger.info("Received phone number: %s", phone_number)
-        
-        # Очистка номера телефона от нечисловых символов
-        cleaned_number = re.sub(r'\D', '', phone_number)
-        
-        # Отправка сообщения
-        message = "Ваше сообщение"
-        send_whatsapp_message(cleaned_number, message)
-        return jsonify({'status': 'success', 'message': 'Message sent successfully'}), 200
+        # Проверка на тестовый запрос от Tilda
+        if request.is_json:
+            data = request.get_json()
+            if isinstance(data, dict) and data.get('test') == 'test':
+                app.logger.info("Received test request from Tilda")
+                return jsonify({'status': 'success', 'message': 'Test request received successfully'}), 200
+        else:
+            # Извлечение JSON данных для обычных запросов
+            data = request.get_json()
+            if not data:
+                app.logger.error("No JSON data found in the request")
+                return jsonify({'status': 'error', 'message': 'Invalid JSON data'}), 400
+
+            app.logger.info("Received JSON data: %s", data)
+
+            # Проверка наличия ключа "Phone" в каждом элементе массива JSON
+            phone_numbers = [item.get('Phone') for item in data if 'Phone' in item]
+
+            if not phone_numbers:
+                app.logger.error("Phone number not found in the JSON data")
+                return jsonify({'status': 'error', 'message': 'Phone number is required'}), 400
+
+            phone_number = phone_numbers[0]  # Берем первый номер телефона из списка
+            app.logger.info("Received phone number: %s", phone_number)
+
+            # Очистка номера телефона от нечисловых символов
+            cleaned_number = re.sub(r'\D', '', phone_number)
+
+            # Отправка сообщения
+            message = "Ваше сообщение"
+            send_whatsapp_message(cleaned_number, message)
+            return jsonify({'status': 'success', 'message': 'Message sent successfully'}), 200
+
     except Exception as e:
         app.logger.error("An error occurred: %s", e, exc_info=True)
         return jsonify({'status': 'error', 'message': 'Internal Server Error'}), 500
 
 def send_whatsapp_message(phone_number, message):
-    url = "https://api.green-api.com/waInstance1103960944/sendMessage/557a5f7c1173434086486f390c6ae2290b77f31ba6ca4656aa"
+    url = "https://api.green-api.com/waInstance1103960944/sendMessage/557a5f7c1173434086486f390c6ae2290b77f31ba6ca4656aaN"
     headers = {
         "Authorization": "Bearer 557a5f7c1173434086486f390c6ae2290b77f31ba6ca4656aa",
         "Content-Type": "application/json"
